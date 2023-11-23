1bbee812333f191314c37f7c09c15072db52f218
chenyouhui1 <chenyouhui1@joyy.com>
2023-11-23 16:21:31 +0800

    语句

--- ts/test_ts.js
+++ ts/test_ts.js
@@ -350,6 +350,21 @@ var test = /** @class */ (function () {
                 }
             }
         }
+        // ------------- 这个写法还挺新颖
+        var num = 25;
+        switch (true) {
+            case num < 0:
+                console.log("Less than 0.");
+                break;
+            case num >= 0 && num <= 10:
+                console.log("Between 0 and 10.");
+                break;
+            case num > 10 && num <= 20:
+                console.log("Between 10 and 20.");
+                break;
+            default:
+                console.log("More than 20.");
+        }
     };
     return test;
 }());
